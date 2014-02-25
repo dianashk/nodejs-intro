e7ac5f9747a6e6d46c4176007711602f703097de
dshkolnikov <dshkolnikov@meetme.com>
2014-02-24 23:29:20 -0500

    step1: create simple server that responds with Hey World! for any request
    Change-Id: I5fb812733bbae88c47baf14dd64325fb47e29d54

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+function onRequest(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hey World');
+  response.end();
+}
+
+http.createServer(onRequest).listen(8888);
+
+
