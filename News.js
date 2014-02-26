6cea1324d2836c7de3fdb04c7322aadecf50b53a
dshkolnikov <dshkolnikov@meetme.com>
2014-02-26 16:38:05 -0500

    adding comments and README
    Change-Id: I45ba1909a8589e93d7a73ca83ef3da3816a38f0b

--- News.js
+++ News.js
@@ -1,18 +1,28 @@
 var EventEmitter = require('events').EventEmitter;
 
+// this is the constructor
 function News(country) {
+  // call the base class constructor
   EventEmitter.call(this);
+
   this.country = country;
 }
 
 // this makes the News class derive from EventEmitter
 News.prototype = Object.create(EventEmitter.prototype);
 
+// class function
 News.prototype.update = function (country, news) {
-  if (country === this.country)
+  if (country === this.country) {
+    // this will trigger the 'update' event and call all registered callbacks
     this.emit('update', news);
-  else
+  }
+  else {
+    // this will trigger the 'error' event and call all registered callbacks
+    // if no callbacks are registered for the 'error' event, this will result in an exception being thrown
     this.emit('error', new Error('not interested in ' + country));
+  }
 };
 
+// export the class
 module.exports = News;
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-nodejs-intro
-============
+# nodejs-intro
+
+## eventEmitter
+
+News broadcast class implemented as an EventEmitter
+
+### to run
+
+  ```npm install colors```
+  ```node server.js```
+
+
+  Try removing the .on('error') handler in server.js and see what happens. Should get an exception.
