6d2be627e228cf05e430a285159841a20a7ca793
dshkolnikov <dshkolnikov@meetme.com>
2014-02-26 16:04:35 -0500

    adding comments and README
    Change-Id: I18431443f77e5343a080087c414bbb8cd27ee919

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-nodejs-intro
-============
+# nodejs-intro
+
+## step1
+
+Basic http server example. Responds to any command with `Hey World!`
+
+### to run
+
+  ```node server.js```
+
+  open ```localhost:8888``` in a browser and see `Hey World!` come back
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+// load the http library, which part of node.js core
 var http = require('http');
 
+// function that will be executed when a request is received
 function onRequest(request, response) {
   console.log('request to url ', request.url);
+
+  // write the response
   response.writeHead(200);
+  // send the response back to the client, asynchronously
   response.end('Hey World');
 }
 
-http.createServer(onRequest).listen(8888);
+// create the server and register the callback function to execute upon incoming requests
+var server = http.createServer(onRequest);
+
+// initiate listening for http requests on port 8888
+server.listen(8888);
 
 
